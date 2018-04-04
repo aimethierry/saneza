c0c95addeea62f520fee9054b79a991f2d73c7e3
aimethierry <aime.thierry97@gmail.com>
2018-04-04 17:10:01 +0200

    changes

--- api/urls.py
+++ api/urls.py
@@ -17,6 +17,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from app.views import ComponycategoryCreateApi, Api
 from app import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -24,3 +26,8 @@ urlpatterns = [
     url(r'^create/category/$', ComponycategoryCreateApi.as_view(), name='post'),
     url(r'^api/$', Api.as_view(), name="Api")
 ]
+
+
+if  settings.DEBUG:
+        urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+        urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
